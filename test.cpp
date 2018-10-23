fa5b35aad0ef0b0825662488971f28761ac42285
Mike Iacovacci <mike.iacovacci@cybereason.com>
2018-10-23 10:33:45 -0700

    Updated CMake and created TestConfig.h.in

--- test.cpp
+++ test.cpp
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "TestConfig.h"
 
-main() {
-    printf("Hello world!");
+int main (int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        fprintf(stdout,"%s Version %d.%d\n",
+                argv[0],
+                testrepo_VERSION_MAJOR,
+                testrepo_VERSION_MINOR);
+        fprintf(stdout,"Usage: %s number\n",argv[0]);
+        return 1;
+    }
+    return 0;
 }
