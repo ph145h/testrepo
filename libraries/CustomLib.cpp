284e68b1b439343fe089aa60d036014f93584b2a
Mike Iacovacci <mike.iacovacci@cybereason.com>
2018-10-28 20:19:11 -0700

    Cross-platform os.h file added

--- libraries/CustomLib.cpp
+++
@@ -1,4 +0,0 @@
-//
-// Created by user on 10/23/18.
-//
-
--- test.cpp
+++ test.cpp
@@ -1,18 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "TestConfig.h"
-#include <windows.h> /* Testing that this breaks macOS and Linux builds... */
+#include "os.h"
+
+const char var1[] = FOO;
 
 int main (int argc, char *argv[])
 {
-    if (argc < 2)
-    {
-        fprintf(stdout,"%s Version %d.%d\n",
-                argv[0],
-                testrepo_VERSION_MAJOR,
-                testrepo_VERSION_MINOR);
-        fprintf(stdout,"Usage: %s number\n",argv[0]);
-        return 1;
-    }
+    fprintf(stdout,"Version: %i.%i\n", testrepo_VERSION_MAJOR, testrepo_VERSION_MINOR);
+    fprintf(stdout,"SYSTEM: %s\n", var1);
     return 0;
 }
